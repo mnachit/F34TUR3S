5c99fd40f9450c642db071b77e88e308c135495f
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-10-19 10:55:05 +0100

    Remove Repetition from UserService

--- gathergrid/src/main/java/com/gathergrid/helpers/user/UserValidationHelper.java
+++ gathergrid/src/main/java/com/gathergrid/helpers/user/UserValidationHelper.java
@@ -1,17 +1,28 @@
 package com.gathergrid.helpers.user;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.gathergrid.entities.User;
+import com.gathergrid.exceptions.costums.ValidationException;
 import com.gathergrid.repository.UserRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 
 public class UserValidationHelper {
 
     private UserRepository userRepository;
+    private Validator validator;
 
     public UserValidationHelper(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     }
 
     protected boolean userNameAlreadyExists(String userName) {
@@ -46,5 +57,17 @@ public class UserValidationHelper {
         String fetchedPassword = fetchedUser.getPassword().getPassword();
 
         return !givedPassword.equals(fetchedPassword);
+    }
+
+    protected <O> void validateObject(O object) {
+
+        Set<ConstraintViolation<O>> violations = validator.validate(object);
+
+        if (!violations.isEmpty()) {
+            List<String> errors = violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toList());
+            throw new ValidationException(errors);
+        }
     }
 }
--- gathergrid/src/main/java/com/gathergrid/service/UserService.java
+++ gathergrid/src/main/java/com/gathergrid/service/UserService.java
@@ -1,44 +1,23 @@
 package com.gathergrid.service;
 
-import com.gathergrid.embeddables.AddressEmail;
-import com.gathergrid.embeddables.Password;
 import com.gathergrid.entities.User;
 import com.gathergrid.exceptions.costums.AlreadyExistsException;
 import com.gathergrid.exceptions.costums.DoNotExistsException;
 import com.gathergrid.exceptions.costums.NotMatchedException;
-import com.gathergrid.exceptions.costums.ValidationException;
 import com.gathergrid.helpers.user.UserValidationHelper;
 import com.gathergrid.repository.UserRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class UserService extends UserValidationHelper {
 
-    private Validator validator;
-
     public UserService() {
         super(new UserRepository());
-        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
     public void registerUser(User user) {
 
-        Set<ConstraintViolation<User>> validationViolations = validator.validate(user);
-
-        if (!validationViolations.isEmpty()) {
-            List<String> errors = validationViolations.stream()
-                    .map(ConstraintViolation::getMessage)
-                    .collect(Collectors.toList());
-
-            throw new ValidationException(errors);
-        }
+        validateObject(user);
 
         if (emailAlreadyExists(user.getEmail().getAddressEmail())) {
             throw new AlreadyExistsException("Email is already exists");
@@ -51,35 +30,19 @@ public class UserService extends UserValidationHelper {
         addAccount(user);
     }
 
-    public void loginUser(User givedUser, HttpServletRequest request) {
+    public void loginUser(User givenUser, HttpServletRequest request) {
 
-        Set<ConstraintViolation<AddressEmail>> emailViolations = validator.validate(givedUser.getEmail());
+        validateObject(givenUser.getEmail());
 
-        if (!emailViolations.isEmpty()) {
-            List<String> errors = emailViolations.stream()
-                    .map(ConstraintViolation::getMessage)
-                    .collect(Collectors.toList());
-
-            throw new ValidationException(errors);
-        }
-
-        Set<ConstraintViolation<Password>> passwordViolations = validator.validate(givedUser.getPassword());
-
-        if (!passwordViolations.isEmpty()) {
-            List<String> errors = passwordViolations.stream()
-                    .map(ConstraintViolation::getMessage)
-                    .collect(Collectors.toList());
-
-            throw new ValidationException(errors);
-        }
-
-        if (noUserHasThisEmail(givedUser.getEmail().getAddressEmail())) {
+        if (noUserHasThisEmail(givenUser.getEmail().getAddressEmail())) {
             throw new DoNotExistsException("This Email Do Not Exist");
         }
 
-        User fetchedUser = getUserByEmail(givedUser.getEmail().getAddressEmail());
+        validateObject(givenUser.getPassword());
 
-        if (passwordsAreNotMatched(givedUser, fetchedUser)) {
+        User fetchedUser = getUserByEmail(givenUser.getEmail().getAddressEmail());
+
+        if (passwordsAreNotMatched(givenUser, fetchedUser)) {
             throw new NotMatchedException("Password Is Incorrect");
         }
 
