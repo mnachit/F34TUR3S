92c71de879e94d238832c250e3b67fb3adff9954
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-10-23 20:15:28 +0100

    Add Javadok documentation to some functions

--- gathergrid/src/main/java/com/gathergrid/service/UserService.java
+++ gathergrid/src/main/java/com/gathergrid/service/UserService.java
@@ -13,52 +13,98 @@ public class UserService extends UserValidationHelper {
         super(new UserRepository());
     }
 
+    /**
+     * Registers a user.
+     *
+     * @param user the user to be registered
+     */
     public void registerUser(User user) {
-
+        // Validate the user
         validateUser(user);
 
+        // Add the user account
         addAccount(user);
     }
 
+    /**
+     * Logs in a user.
+     * 
+     * @param givenUser The user to be logged in.
+     * @param request   The HTTP servlet request object.
+     */
     public void loginUser(User givenUser, HttpServletRequest request) {
 
+        // Validate the email format
         validateEmail(givenUser.getEmail());
 
+        // Fetch the user by email
         User fetchedUser = getUserByEmail(givenUser.getEmail().getAddressEmail());
 
+        // Validate the user's password
         validatePassword(givenUser, fetchedUser);
 
+        // Store the logged-in user in the session
         storeLoggedUserInSession(fetchedUser, request);
-
     }
 
-    public void updateProfile(User givenUser, HttpServletRequest request) {
+/**
+ * Updates the profile of a given user.
+ *
+ * @param givenUser The user object with the updated profile information.
+ * @param request   The HTTP servlet request object.
+ */
+public void updateProfile(User givenUser, HttpServletRequest request) {
 
-        validateUserOnUpdate(givenUser, request);
+    // Validate the given user on update
+    // Validate the given user object for any update errors
+    validateUserOnUpdate(givenUser, request); 
 
-        User fetchedUser = getUserById(getStoredLoggedUserFromSession(request).getId());
+    // Get the logged-in user from the session by their ID
+    User fetchedUser = getUserById(getStoredLoggedUserFromSession(request).getId()); 
 
-        validatePassword(givenUser, fetchedUser);
+    // Validate the given user's password against the fetched user's password
+    validatePassword(givenUser, fetchedUser); 
 
-        User updatedUser = updateAccount(givenUser, request);
+    // Update the user's account with the given user's profile information
+    User updatedUser = updateAccount(givenUser, request); 
 
-        updateLoggedUserInSession(updatedUser, request);
-    }
+    // Update the logged-in user in the session with the updated user object
+    updateLoggedUserInSession(updatedUser, request); 
+}
 
-    public void changePassword(String currentPassword, String newPassword, String repeatNewPassword,
-            HttpServletRequest request) {
+/**
+ * Changes the password for the current user.
+ *
+ * @param currentPassword   The current password of the user.
+ * @param newPassword       The new password to set.
+ * @param repeatNewPassword The repeated new password for confirmation.
+ * @param request           The HTTP servlet request.
+ */
+public void changePassword(String currentPassword, String newPassword, String repeatNewPassword,
+        HttpServletRequest request) {
 
-        User user = getUserById(getStoredLoggedUserFromSession(request).getId());
+    // Get the current user object by retrieving the logged-in user from the session
+    User user = getUserById(getStoredLoggedUserFromSession(request).getId());
 
-        validatePasswords(user.getPassword().getPassword(), currentPassword, newPassword, repeatNewPassword);
+    // Validate the passwords by comparing the current password with the provided current password
+    // and checking if the new password and the repeat new password match
+    validatePasswords(user.getPassword().getPassword(), currentPassword, newPassword, repeatNewPassword);
 
-        user.setPassword(new Password(newPassword));
+    // Set the new password for the user
+    user.setPassword(new Password(newPassword));
 
-        updateAccount(user, request);
-    }
+    // Update the user account with the new password
+    updateAccount(user, request);
+}
 
-    public void logoutUser(HttpServletRequest request) {
-        request.getSession().removeAttribute("LoggedUser");
-    }
+/**
+ * Method to log out a user.
+ *
+ * @param request the HttpServletRequest object containing the user's request
+ */
+public void logoutUser(HttpServletRequest request) {
+    // Remove the "LoggedUser" attribute from the session
+    request.getSession().removeAttribute("LoggedUser");
+}
 
 }
